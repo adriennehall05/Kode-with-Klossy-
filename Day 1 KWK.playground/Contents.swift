583a92b8827b830e19b944d8ce40b7a9b4f68d3e
Apple <apple@Apples-MacBook-Pro-122.local>
2019-07-22 16:40:25 -0400

    good job today

--- Day 1 KWK.playground/Contents.swift
+++ Day 1 KWK.playground/Contents.swift
@@ -3,6 +3,7 @@ import UIKit
 var str = "Hello, playground"
 str
 
+
 var name = "Adrienne"
 name
 name = "Adri"
@@ -78,3 +79,93 @@ if github == "Rocks" {
 else {
     print("Try Again")
 }
+
+//FUNCTIONS
+
+//Walking a dog
+//1. Call dog over
+//2. Get the collar
+//3. Get the leash
+//4. Buckle leash onto the collar
+//5. Open Door
+//6. Step outside door
+//7. Start Walking
+
+func walkDog(){
+    print("1. call dog over")
+    print("2. get the collar")
+    print("3. get the leash")
+    print("4. buckle leash onto collar")
+    print("5. open door")
+    print("6. step outside door")
+    print("7. start walking")
+}
+walkDog()
+
+//Making Cereal
+//1. Get a bowl
+//2. Get the cereal box
+//3. Open the Cereal box and pour cereal into bowl
+//4. Close the cereal box and put it away
+//5. Get milk
+//6. Pour milk into bowl with cereal
+//7. Put milk away
+//8. Grab a spoon
+//9. Enjoy!!
+
+func makingCereal (){
+    print("1. Get a bowl")
+    print("2. Get the cereal box")
+    print("3. Open the cereal box abd pour cereal into bowl")
+    print("4. Close the cereal box and put it away")
+    print("5. Get milk")
+    print("6. Pour milk into bowl with cereal")
+    print("7. Put milk away")
+    print("8. Grab a spoon")
+    print("9. Enjoy!!")
+}
+makingCereal()
+
+//Arguments
+//func walkDog(numberOfDogs: Int) {
+//    print("There are \(numberOfDogs) dogs in the house")
+//}
+//walkDog(numberOfDogs : 4)
+
+//Argument Exersice
+func hello(name: String) {
+    print("hello, \(name)")
+}
+hello(name: "adrienne")
+//^make sure your writing String not str
+//^if you want to change the name in the 135th line make sure its "name" not your actual name
+//^you can change the name in the 138th line of code.
+
+func walkDog(numberOfDogs : Int) -> Int {
+let lengthOfWalk = numberOfDogs * 15
+return lengthOfWalk
+}
+let minutesToWalk = walkDog(numberOfDogs : 3)
+print("Please walk the dogs. I will expect you to see you in complete that task in \(minutesToWalk) minutes!")
+//The let was used because minutes to walk is not changes. If you are curious, try starting with var then noticing the Xcode throws.
+
+//COLLECTIONS
+var friendsOfKarlie = ["Michelle Obama", "Serena Williams", "T Swift", "Jimmy Falon"]
+
+friendsOfKarlie[2]
+friendsOfKarlie[0]
+print(friendsOfKarlie[3])
+//^ Each name had an index, since michelle had 0, when putting friendsOfKarlie, then 0 in brackets, in the working side it show michelle obama.
+friendsOfKarlie[2] = "Josh Kushner"
+print(friendsOfKarlie)
+//^check
+// to add new information to array, put .append
+//to remove information from array, put .remove
+
+var roleModelsOfAdrienne = ["Blake Bolden", "Caitrin Lonergan", "Alex Morgan" ]
+
+roleModelsOfAdrienne[1]
+roleModelsOfAdrienne.append ("Bella Vassuer")
+roleModelsOfAdrienne.remove (at : 0)
+roleModelsOfAdrienne[0] = "BB athletics"
+print(roleModelsOfAdrienne)
