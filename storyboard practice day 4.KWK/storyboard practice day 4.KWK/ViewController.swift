27128f17f555b3601ea26861ef399f326e5e360a
Apple <apple@Apples-MacBook-Pro-122.local>
2019-07-25 14:16:26 -0400

    new

--- storyboard practice day 4.KWK/storyboard practice day 4.KWK/ViewController.swift
+++ storyboard practice day 4.KWK/storyboard practice day 4.KWK/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pageTitle: UILabel!
+    
+    @IBOutlet weak var pageText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+   
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+    
+    if let newTitle = pageText.text    {
+            pageTitle.text = newTitle
+        }
 
 }
-
+}
